4e131e68c19d5d9761ee57d13bc93533e71ff869
jameswilddev <jameswilddev@outlook.com>
2022-05-12 23:44:19 +0100

    Add test coverage.

--- tests/run.bash
+++ tests/run.bash
@@ -39,3 +39,23 @@
   diff --brief --recursive $actual $expected
   rm -rf $temporaryDirectory
 }
+
+@test "no changes" {
+  repository=$(pwd)
+  temporaryDirectory=$(mktemp -d)
+  expected=$temporaryDirectory/expected
+  cp -r ./tests/no-changes/expected/. $expected
+  mkdir -p $expected/submodules/dreck
+  cp -r . $expected/submodules/dreck
+  actual=$temporaryDirectory/actual
+  cp -r ./tests/no-changes/input/. $actual
+  mkdir -p $actual/submodules/dreck
+  cp -r . $actual/submodules/dreck
+  cd $actual
+
+  make --file ./submodules/dreck/makefile
+
+  cd $repository
+  diff --brief --recursive $actual $expected
+  rm -rf $temporaryDirectory
+}
