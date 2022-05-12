5003d7b068c96074e6e694f1453e0ca382418296
jameswilddev <jameswilddev@outlook.com>
2022-05-12 23:06:08 +0100

    Add first run tests.

--- tests/run.bash
+++ tests/run.bash
@@ -1,5 +1,25 @@
 #!/usr/bin/env bats
 
+@test "first run" {
+  repository=$(pwd)
+  temporaryDirectory=$(mktemp -d)
+  expected=$temporaryDirectory/expected
+  cp -r ./tests/first-run/expected/. $expected
+  mkdir -p $expected/submodules/dreck
+  cp -r . $expected/submodules/dreck
+  actual=$temporaryDirectory/actual
+  cp -r ./tests/first-run/input/. $actual
+  mkdir -p $actual/submodules/dreck
+  cp -r . $actual/submodules/dreck
+  cd $actual
+
+  make --file ./submodules/dreck/makefile
+
+  cd $repository
+  diff --brief --recursive $actual $expected
+  rm -rf $temporaryDirectory
+}
+
 @test "subsequent run" {
   repository=$(pwd)
   temporaryDirectory=$(mktemp -d)
